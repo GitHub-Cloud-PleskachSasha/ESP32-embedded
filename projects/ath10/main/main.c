0616035bee6bd96cee93720ed55bf05f43ec3e39
PleskachSasha <9pleskach9@gmail.com>
2025-05-18 22:23:00 +0300

    ath10: add ath measure

--- projects/ath10/main/main.c
+++ projects/ath10/main/main.c
@@ -2,17 +2,16 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 
-#define I2C_PORT I2C_NUM_0  // чому він і я зробив на початку без нього 
-    // ESP_ERROR_CHECK(i2c_param_config(I2C_MODE, &i2c_congfig));
-    // ESP_ERROR_CHECK(i2c_driver_install(I2C_MODE, i2c_congfig.mode, 0, 0, 0));
+#define I2C_PORT I2C_NUM_0  //
 #define I2C_MODE I2C_MODE_MASTER
 #define I2C_NUM_SDA 33
 #define I2C_NUM_SDL 34
 #define I2C_HZ 100000
 
 #define AHT10_I2C_ADDR 0x38
-#define AHT10_I2C_INIT 0xE1 //
-#define AHT10_I2C_CALIBR 0x08 // 
+#define AHT10_I2C_INIT 0xE1 
+#define AHT10_I2C_CALIBR 0x08 //
+#define AHT10_I2C_MEASURE 0xAC  
 
 static const char* TAG = "AHT10";
 
@@ -43,7 +42,7 @@ esp_err_t i2c_init(i2c_config_t *i2c_config_out) {
 }
 
 esp_err_t aht10_init() {
-    uint8_t init_cmd[] = {AHT10_I2C_INIT, AHT10_I2C_CALIBR, 0x00};
+    uint8_t init_cmd[] = {AHT10_I2C_INIT, AHT10_I2C_CALIBR, 0x00}; //
     esp_err_t err = i2c_master_write_to_device(
         I2C_NUM_0,
         AHT10_I2C_ADDR,
@@ -58,6 +57,44 @@ esp_err_t aht10_init() {
         ESP_LOGE(TAG, "aht10 init failed: %s", esp_err_to_name(err));
         return err;
     }
+
+    return ESP_OK;
+}
+
+esp_err_t aht10_measure() {
+    uint8_t init_cmd[] = {AHT10_I2C_MEASURE, 0x33, 0x00}; //
+    esp_err_t err = i2c_master_write_to_device(
+        I2C_NUM_0,
+        AHT10_I2C_ADDR,
+        init_cmd,
+        sizeof(init_cmd),
+        1000 / portTICK_PERIOD_MS
+    );
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "aht10 measure init failed: %s", esp_err_to_name(err));
+        return err;
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+
+    uint8_t data[6];
+    esp_err_t data_err = i2c_master_read_from_device(
+        I2C_NUM_0,
+        AHT10_I2C_ADDR,
+        data,
+        sizeof(datai2c_master_write_to_deviceESP_OK) {
+        ESP_LOGE(TAG, "aht10 get data failed: %s", esp_err_to_name(data_err));
+        return data_err;
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+ 
+    uint32_t raw_humidity = (data[1] << 12) | (data[2] << 4) | (data[3] >> 4); //
+    uint32_t raw_temperature = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]; //
+
+    float humidity = ((float)raw_humidity / 1048576.0) * 100.0;
+    float temperature = ((float)raw_temperature / 1048576.0) * 200.0 - 50.0;
+
+    ESP_LOGI(TAG, "Temperature: %.2f °C, Humidity: %.2f %%", temperature, humidity);
 
     return ESP_OK;
 }
@@ -77,10 +114,8 @@ void app_main() {
     }
 
     ESP_LOGI(TAG, "Connect is ok");
-    // while (true)
-    // {
-        // float humidity = 0.0;
-        // float temperature = 0.0;
-    // }
-    
+    while (true)
+    {
+        aht10_measure();
+    }
 }
